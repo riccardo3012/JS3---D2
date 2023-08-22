a797efcfa52f2e9faf13d305655a943f56645a23
riccardo3012 <ronca.rcr@gmail.com>
2023-08-22 16:57:09 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const nome = document.getElementById("nome");
+let save = document.getElementById("salva");
+const nomeSalvatoElement = document.getElementById("nome-salvato");
+
+save.addEventListener("click", function () {
+  const nomeInserito = nome.value;
+  localStorage.setItem("nome", nomeInserito);
+  nomeSalvatoElement.textContent = "Nome Salvato: " + nomeInserito;
+});
+
+document.getElementById("elimina").addEventListener("click", function () {
+  localStorage.removeItem("nome");
+  nome.value = "";
+  nomeSalvatoElement.textContent = "";
+});
+
+const nomeSalvato = localStorage.getItem("nome");
+if (nomeSalvato) {
+  nome.value = nomeSalvato;
+  nomeSalvatoElement.textContent = "Nome Salvato: " + nomeSalvato;
+}
+
+function startTimer() {
+  let startTime = 0;
+  const savedTime = sessionStorage.getItem("timer");
+  if (savedTime) {
+    startTime = parseInt(savedTime, 10);
+  }
+
+  const timerElement = document.getElementById("timer");
+  const timerInterval = setInterval(function () {
+    startTime++;
+    timerElement.textContent = startTime + " secondi";
+    sessionStorage.setItem("timer", startTime.toString());
+  }, 1000);
+
+  timerElement.textContent = startTime + " secondi";
+}
+
+window.addEventListener("load", startTimer);
